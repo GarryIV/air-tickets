fd1846c8e06126c4a51c04a7e7419d6ec4456257
GarryIV <garryiv@gmail.com>
2016-08-05 22:38:50 +0300

    core

--- src/air-tickets/air-tickets-core/src/main/java/com/garryiv/air_tickets/core/auth/CoreSecurityConfiguration.java
+++ src/air-tickets/air-tickets-core/src/main/java/com/garryiv/air_tickets/core/auth/CoreSecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -28,6 +29,7 @@ import java.util.stream.Collectors;
 
 @Configuration
 @EnableOAuth2Client
+@EnableAuthorizationServer
 @Order(6)
 public class CoreSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -44,13 +46,18 @@ public class CoreSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                     .authenticated()
                 .and()
-                    .exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
+                    .exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
                 .and()
                     .logout().logoutSuccessUrl("/").permitAll()
                 .and()
                     .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
                     .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+
+        http
+                .formLogin()
+                .loginProcessingUrl("/login/staff");
+
     }
 
     @Bean
