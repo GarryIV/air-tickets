b33b320434ae263ebd3f38defac5ddeabe80f74b
GarryIV <garryiv@gmail.com>
2016-08-07 15:06:20 +0300

    menu

--- src/air-tickets/air-tickets-ui/src/main/resources/static/js/flight/flight.js
+++ src/air-tickets/air-tickets-ui/src/main/resources/static/js/flight/flight.js
@@ -1,16 +1,31 @@
 angular.module('flight', []).controller('flight', function($http) {
     var flight = {
         filter: {
-            from: 'CDG',
-            to: 'JFK',
+            origin: 'CDG',
+            destination: 'JFK',
             departure: new Date()
         },
         flights: null
     };
 
+    var addDays = function(date, days) {
+        var result = new Date(date);
+        result.setDate(result.getDate() + days);
+        return result;
+    }
+
     flight.search = function () {
-        $http.post('/api/flight/search', flight.filter).then(function () {
-            flight = response.data;
+        var filter = flight.filter;
+        var from = new Date(filter.departure.setHours(0,0,0,0));
+        var to = addDays(from, 1);
+
+        $http.post('/api/flight/search', {
+            origin: filter.origin,
+            destination: filter.destination,
+            departureFrom: from,
+            departureTo: to
+        }).then(function (response) {
+            flight.flights = response.data;
         })
     };
 
